3dca201188372889607a56a4256ca1f918adc8b3
rubenlux <ruben.luxor2012@gmail.com>
2026-04-07 19:38:11 -0300

    fix: restore readFile export in api.js to fix frontend build

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -290,9 +290,9 @@ export const listFiles = async (hostingId, path = '') => {
     return response.data;
 };
 
-export const readFiles = async (hostingId, path = '') => {
-  const response = await api.get(`/hosting/${hostingId}/fs/read`, { params: { path } });
-  return response.data.content;
+export const readFile = async (hostingId, path) => {
+    const response = await api.get(`/files/${hostingId}/read`, { params: { path } });
+    return response.data;
 };
 
 export const diagnoseHosting = async (hostingId) => {
